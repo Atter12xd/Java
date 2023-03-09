701c99cca8fc137c279167c6f33d310a4c0a7c9a
Atter12xd <1485275@senati.pe>
2023-03-09 18:46:20 -0500

    Imprimimos_Numero_3

--- practicaJava.java
+++ practicaJava.java
@@ -1,3 +1,8 @@
 /* Creamos la clase del archivo en esta caso 
 "practicaJava" */
-public class practicaJava
+public class practicaJava {
+    public static void main(String args) {
+        System.out.println(3); // creamos un numero
+    }
+
+}
