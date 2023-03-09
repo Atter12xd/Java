e4b87293bfa0c4a48f2dc40fdb7398695fabd9b5
Atter12xd <1485275@senati.pe>
2023-03-09 18:48:34 -0500

    Buscando error y corregiendolo

--- practicaJava.java
+++ practicaJava.java
@@ -1,7 +1,8 @@
 /* Creamos la clase del archivo en esta caso 
 "practicaJava" */
 public class practicaJava {
-    public static void main(String args) {
+    public static void main(String[] args)/* me falto poner "[]" */
+    {
         System.out.println(3); // creamos un numero
     }
 
