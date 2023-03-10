fb4ea18ffe89cac25805eb218f85944f6094d20c
Atter12xd <1485275@senati.pe>
2023-03-09 19:18:57 -0500

    Cambiamos el numero del anterior variable

--- practicaJava.java
+++ practicaJava.java
@@ -1,12 +1,14 @@
 /* Creamos la clase del archivo en esta caso 
 "practicaJava" */
+
 public class practicaJava {
     public static void main(String[] args) {
         System.out.println(3); // creamos un numero
-        System.out.println("Atter Basilio"); // Recordemos siemopre terminar con ";"
+        System.out.println("Atter Basilio"); // Recordemos siempre terminar con ";"
         String variable = "hola mundo"; // creamos una variable usando string
         System.out.println(variable); // ejecutamos la varibale
         int numero = 20; // creamos una variable usando int
+        numero = 60; // cambiamos el numero del anterior codigo
         System.out.println(numero); // ejecutamos la variable
     }
 
