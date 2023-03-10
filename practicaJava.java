23879aa3b58503587e216c4edc2d751e7c0f9c7b
Atter12xd <1485275@senati.pe>
2023-03-09 19:00:11 -0500

    Creamos una variable usando string y ejecutamos

--- practicaJava.java
+++ practicaJava.java
@@ -1,10 +1,11 @@
 /* Creamos la clase del archivo en esta caso 
 "practicaJava" */
 public class practicaJava {
-    public static void main(String[] args)/* me falto poner "[]" */
-    {
+    public static void main(String[] args) {
         System.out.println(3); // creamos un numero
         System.out.println("Atter Basilio"); // Recordemos siemopre terminar con ";"
+        String variable = "hola mundo"; // creamos una variable usando string
+        System.out.println(variable); // ejecutamos la varibale
     }
 
 }
